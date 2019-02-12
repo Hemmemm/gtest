5f721cc36aaab5b2cbc04601a594c848cd6ec89a
Egor Ignatov <ignatov@cs.petrsu.ru>
2019-02-12 13:04:21 +0300

    Added some tests
    Signed-off-by: Egor Ignatov <ignatov@cs.petrsu.ru>

--- tests/findroots_test.h
+++ tests/findroots_test.h
@@ -8,7 +8,7 @@ extern "C"
 #include "findroots.h"
 }
 
-TEST(rootstest, disEq0)
+TEST(rootstest, disEqual0)
 {
     Roots check;
     check.x1 = -1.0;
@@ -17,6 +17,27 @@ TEST(rootstest, disEq0)
     Roots res = findRoots(1.0, 2.0, 1.0);
     ASSERT_DOUBLE_EQ(check.x1, res.x1);
     ASSERT_DOUBLE_EQ(check.x2, res.x2);
+    ASSERT_EQ(check.found, res.found);
+}
+
+TEST(rootstest, disGreater0)
+{
+    Roots check;
+    check.x1 = 4.0;
+    check.x2 = -2.0;
+    check.found = 1;
+    Roots res;
+    res = findRoots(1.0, -2.0, -8.0);
+    ASSERT_DOUBLE_EQ(check.x1, res.x1);
+    ASSERT_DOUBLE_EQ(check.x2, res.x2);
+    ASSERT_EQ(check.found, res.found);
+}
+
+TEST(rootstest, disLess0)
+{
+    Roots check;
+    check.found = 0;
+    Roots res = findRoots(1.0, -1.0, 1.0);
     ASSERT_EQ(check.found, res.found);
 }
 
